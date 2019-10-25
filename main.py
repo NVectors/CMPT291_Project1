b8bf7b57e5d55bcb34d3b8fcb14108db8232ff77
Victor <victor3@ualberta.ca>
2019-10-24 20:34:41 -0600

    Added database connection to main.py

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import sys
 import sqlite3
 import Queries
 
+
 def main():
-    connection, cursor = Queries.connect('p1.db')
+    try:
+        database_file = 'p1.db'  # Once final, edit to sys.argv[] to run in Terminal
+    except:
+        raise Exception("Database file path not defined")
+    connection, cursor = Queries.connect(database_file)
 
 
+if __name__ == "__main__":
+    main()
